cb702fdb00d44a6f39e5881df4b0c0736b89f464
Zain~Sama <abdulrahmanzain32@gmail.com>
2024-09-06 04:43:00 +0200

    experimental animation

---
+++ script.js
@@ -0,0 +1,13 @@
+// const button = document.querySelector('button');
+
+// function enlarge() {
+//     button.style.transform = "scale(1.1)";
+// };
+
+// function small() {
+//     button.style.transform = "scale(1)"
+// };
+
+// button.addEventListener('mouseover', enlarge);
+
+// button.addEventListener('mouseout', small)
